a985950907b933e0390e3e7286cf617b9eacf9d8
Arina Shmodina <ApelsinGilka@yandex.ru>
2026-06-09 21:21:47 +0300

    fix isPrime prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,14 +3,14 @@ import { getRandomNum } from '../cli.js'
 const runPrimeGame = () => {
   const randomNum = getRandomNum()
 
-  const isPrime = () => {
-    if (randomNum < 2) return false
-    if (randomNum === 2) {
+  const isPrime = (num) => {
+    if (num < 2) return false
+    if (num === 2) {
       return true
     }
-    if (randomNum % 2 === 0) return false
-    for (let i = 3; i < Math.sqrt(randomNum); i += 2) {
-      if (randomNum % i === 0) return false
+    if (num % 2 === 0) return false
+    for (let i = 3; i < Math.sqrt(num); i += 2) {
+      if (num % i === 0) return false
     }
     return true
   }
