d8144762a9bc3e71af2d1c70f46e4963b1614729
Arina Shmodina <ApelsinGilka@yandex.ru>
2026-06-10 09:39:50 +0300

    fix isPrime prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,18 +5,15 @@ const runPrimeGame = () => {
 
   const isPrime = (num) => {
     if (num < 2) return false
-    if (num === 2) {
-      return true
-    }
+    if (num === 2) return true
     if (num % 2 === 0) return false
-    for (let i = 3; i <= i * i <= num; i += 2) {
+    for (let i = 3; i * i <= num; i += 2) {
       if (num % i === 0) return false
     }
     return true
   }
 
   const correctAnswer = isPrime(randomNum) ? 'yes' : 'no'
-
   return { question: randomNum, correctAnswer }
 }
 
